5cb34903fd26b165cb726e5bd019800de2f79778
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-05-15 17:27:42 +0100

    finish store config

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rocketsReducer from './Rockets/rocketsSlice';
+import missionsReducer from './Missions/missionsSlice';
 
 const store = configureStore({
   reducer: {
     rockets: rocketsReducer,
+    missions: missionsReducer,
   },
 });
 
