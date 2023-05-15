d5d32c2f302d3faab7e8f3b364c5eaf1bb07c019
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-05-15 22:01:44 +0100

    display the rockets from the API and add basic styling

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,9 +1,38 @@
-const Rockets = () => (
-  <div>
-    <p>
-      Rockets
-    </p>
-  </div>
-);
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { fetchRockets } from '../redux/Rockets/rocketsSlice';
+
+const Rockets = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, []);
+  const { rocketsList, isLoading } = useSelector((store) => store.rockets);
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <section>
+      {rocketsList.map((rocket) => (
+        <div key={rocket.id} className={`card ${rocket.id}`}>
+          <div className="image-container">
+            <img src={rocket.flickr_images[0]} alt="Rocket" />
+          </div>
+          <div className="content">
+            <h3 className="title">{rocket.name}</h3>
+            <p className="description">{rocket.description}</p>
+            <button type="submit" className={`reservation-button btn-${rocket.id}`}>Reserve Now</button>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+};
 
 export default Rockets;
