58e86d944db2400fe17a7a13064c4c8e47f2e585
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-05-15 12:02:21 +0100

    create components folder

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => (
+    <nav>
+        <div className="logo-title">
+            <img className="logo" src="src\components\assets\planet.png" alt="Planet logo" />
+            <h1 className="title">Space traveler's Hub</h1>
+        </div>
+        <ul>
+            <li>Rockets</li>
+            <li>Missions</li>
+            <hr />
+            <li>My profile</li>
+        </ul>
+    </nav>
+)
