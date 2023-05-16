4934b4e7429eef52c6c1d6494a37163aea25fc62
yacoubou-seidou <yacoseidou@gmail.com>
2023-05-16 15:36:17 +0100

    Show joined missions on profile page, fix some styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import {
   BrowserRouter, Routes, Route,
 } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getMissions } from './redux/Missions/missionsSlice';
 import Navbar from './components/Navbar';
-import Myprofile from './components/Myprofile';
+import MyProfile from './components/Myprofile';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getMissions());
+  }, [dispatch]);
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Rockets />} />
         <Route path="/missions" element={<Missions />} />
-        <Route path="/profile" element={<Myprofile />} />
+        <Route path="/profile" element={<MyProfile />} />
       </Routes>
     </BrowserRouter>
   );
