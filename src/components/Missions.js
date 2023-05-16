fec14b1bee7e15ffeeb26be19a9dbff4d3f87bc4
yacoubou-seidou <yacoseidou@gmail.com>
2023-05-16 12:22:29 +0100

    Feth Missions and display them

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,9 +1,52 @@
-const Missions = () => (
-  <div>
-    <p>
-      Missions
-    </p>
-  </div>
-);
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getMissions } from '../redux/Missions/missionsSlice';
+
+const Missions = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getMissions());
+  }, [dispatch]);
+  const missions = useSelector((store) => store.missions);
+  return (missions && missions.missions.length > 0 ? (
+    <>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th> </th>
+
+          </tr>
+        </thead>
+        <tbody>
+          {missions.missions.map((mission) => (
+            <tr key={mission.mission_id}>
+              <td className="name">{mission.mission_name}</td>
+              <td className="description">{mission.description}</td>
+              <td className="status">
+                {mission.reserved ? (
+                  <span className="active_member">Active Member</span>
+                ) : (
+                  <span className="not_a_member">Not A Member</span>
+                )}
+
+              </td>
+              <td>
+                {mission.reserved ? (
+                  <button type="button" className="btn_leave_mission">Leave Mission</button>
+                ) : (
+                  <button type="button" className="btn_join_mission">Join Mission</button>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  ) : <p>Loading...</p>
+  );
+};
 
 export default Missions;
